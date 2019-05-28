0bcbd21348b90d865e94fc1d257f59f95c8a0e5b
connormatheny <mathenc@miamioh.edu>
2019-05-28 14:59:54 -0400

    try to push for server

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -2,12 +2,12 @@ import React from "react";
 export default class Home extends React.Component{
     render(){
         return(
-            <div>
+            <div style={{ display:'flex', flexDirection:'column', justifyContent:'center', alignItems: 'center'}}>
                 <div className="text-center">
-                    <h1 style={{marginTop:100, marginBottom: 20}}>Welcome!</h1>
-                    <h1>This site was created to help you find something to watch</h1>
+                    <h1 style={{marginTop:30, marginBottom: 20}}>Welcome!</h1>
+                    <h1>This site was created to help you find something to watch.</h1>
                 </div>
-                <div>
+                <div style={{marginTop:30, marginBottom: 20, width:'50%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', fontSize:16}}>
                     <h2>Instructions if you really need them: </h2>
                     <p>If you have no idea what you want to watch, head to the platform you'd like to watch on and then generate a random movie or show title.</p>
                     <p>If you have a particular show already in mind, head to the search page and type in the name of the show you want to watch. Once you've found the correct title, click on the dropdown and hit the random episode button.</p>
